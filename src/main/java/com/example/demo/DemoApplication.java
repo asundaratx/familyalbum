7091b29d20000c44a2373838a44b856d8e2b12d1
Aparna Sundar <sundar.aparna@gmail.com>
2020-05-12 13:43:05 -0500

    Moving secrets to environment variables, extend authentication to include local db

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,6 +22,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -26,6 +35,8 @@ public class DemoApplication extends WebSecurityConfigurerAdapter {
 	UserRepository userRepository;
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
 		// @formatter:off
 		http
 				.authorizeRequests(a -> a
@@ -41,8 +52,21 @@ public class DemoApplication extends WebSecurityConfigurerAdapter {
 				.exceptionHandling(e -> e
 						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
 				)
-				.oauth2Login();
+				.oauth2Login()
+				.failureHandler((request, response, exception) -> {
+					request.getSession().setAttribute("error.message", exception.getMessage());
+					handler.onAuthenticationFailure(request, response, exception);
+				});
 		// @formatter:on
+	}
+
+	@GetMapping("/error")
+	public String error(HttpServletRequest request) {
+		String message = (String) request.getSession().getAttribute("error.message");
+		if(message!=null)
+			message.concat(" To obtain authorization or check errors, use <a href=\"mailto:sundar.aparna@gmail.com?subject=AuthorizationRequest\">Contact admin </a>");
+		request.getSession().removeAttribute("error.message");
+		return message;
 	}
 
 	@GetMapping("/user")
@@ -54,7 +78,6 @@ public class DemoApplication extends WebSecurityConfigurerAdapter {
 		if(email!=null) {
 			User user = userRepository.findByEmail(email);
 			if(user!=null) {
-				log.info("This is an authorized user");
 				dataMap.put("authorized", Boolean.TRUE);
 			} else {
 				dataMap.put("unauthorized", Boolean.TRUE);
@@ -62,6 +85,24 @@ public class DemoApplication extends WebSecurityConfigurerAdapter {
 		}
 		return dataMap;
 	}
+
+	@Bean
+	public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+		DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+		return request -> {
+			User appUser;
+			OAuth2User user = delegate.loadUser(request);
+			String email = user.getAttribute("email");
+			if (email!=null) {
+				appUser = userRepository.findByEmail(email);
+				if (appUser!=null) {
+					return user;
+				}
+			}
+			throw new OAuth2AuthenticationException(new OAuth2Error("invalid_token", "Not in app user list", ""));
+		};
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
